784c32642fb557e2c6ac67f6031ebe5edce6461d
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-04 10:16:46 +0100

    continuation

---
+++ exercises.py
@@ -0,0 +1,16 @@
+#Stride length
+to_one_hundred = range(101)
+backwards_by_tens = to_one_hundred[::-10]
+
+print(backwards_by_tens)
+
+
+#Practice makes perfect
+to_21 = range(1, 22)
+odds = to_21[::2]
+middle_third = to_21[7:14]
+
+
+#Anonymous functions
+my_list = range(16)
+print filter(lambda x: x % 3 == 0, my_list)

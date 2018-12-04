d22748cc2968c7e6f7e28050fe9cef63e5c92ac8
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-04 12:52:08 +0100

    continuation

--- exercises.py
+++ exercises.py
@@ -25,5 +25,6 @@ squares = [x ** 2 for x in range(1, 11)]
 print(list(filter(lambda x: 29 < x < 71 , squares)))
 
 
-#
-
+#Comprehending comprehensions
+threes_and_fives = [x for x in range(1, 16) if x % 3 == 0 or x % 5 == 0]
+print(threes_and_fives)

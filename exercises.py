27f8bdfe97e74a71a0c0e09819ec40853786e26c
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-04 10:47:40 +0100

    continuation

--- exercises.py
+++ exercises.py
@@ -14,3 +14,9 @@ middle_third = to_21[7:14]
 #Anonymous functions
 my_list = range(16)
 print filter(lambda x: x % 3 == 0, my_list)
+
+
+#Lambda syntax
+languages = ["HTML", "JavaScript", "Python", "Ruby"]
+print filter(lambda x: x == 'Python' , languages)
+

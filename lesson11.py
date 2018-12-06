fab4363e561d81741ba20e07795a3ef583c398c6
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-06 11:04:35 +0100

    continuation

--- lesson11.py
+++ lesson11.py
@@ -19,3 +19,18 @@ class Animal():
 
 zebra = Animal('Jeffrey')
 print(zebra.name)
+
+#More on __init__() and self
+class Animal(object):
+    def __init__(self, name, age, is_hungry):
+        self.name = name
+        self.age = age
+        self.is_hungry = is_hungry
+        
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print(zebra.name, zebra.age, zebra.is_hungry)
+print(giraffe.name, giraffe.age, giraffe.is_hungry)
+print(panda.name, panda.age, panda.is_hungry)

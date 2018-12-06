df0d3622861345b7e84c52d2b4e9770707a6f72d
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-06 10:01:16 +0100

    continuation

--- lesson11.py
+++ lesson11.py
@@ -1,3 +1,11 @@
 #Class syntax
-class Animal(object):
+class Animal():
     pass
+#Classier Classes
+class Animal():
+    def __init__(self):
+        pass
+#Let's not get too selfish
+class Animal():
+    def __init__(self, name):
+        self.name = name

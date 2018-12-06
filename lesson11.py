3cae680286a163825c9047cbb514a85087997fc5
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-06 10:43:28 +0100

    continuation

--- lesson11.py
+++ lesson11.py
@@ -1,11 +1,21 @@
 #Class syntax
 class Animal():
     pass
+
 #Classier Classes
 class Animal():
     def __init__(self):
         pass
+
 #Let's not get too selfish
 class Animal():
     def __init__(self, name):
         self.name = name
+
+#Instantiating Your First Object
+class Animal():
+    def __init__(self, name):
+        self.name = name
+
+zebra = Animal('Jeffrey')
+print(zebra.name)

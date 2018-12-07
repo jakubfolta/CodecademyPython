c3dfb1eb4703914f4f76302dbaf488d2ba436bcb
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-07 04:48:07 +0100

    continuation

--- lesson11.py
+++ lesson11.py
@@ -36,4 +36,16 @@ print(giraffe.name, giraffe.age, giraffe.is_hungry)
 print(panda.name, panda.age, panda.is_hungry)
 
 #Class Scope
+class Animal(object):
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+zebra = Animal('Jeffrey', 2)
+giraffe = Animal('Bruce', 1)
+panda = Animal('Po', 7 )
+
+print(zebra.name, zebra.age, zebra.is_alive)
+print(giraffe.name, giraffe.age, giraffe.is_alive)
+print(panda.name, panda.age, panda.is_alive)

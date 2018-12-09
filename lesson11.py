7bc7a8c6cad697ff7aa73aefb9259d0391a3ac00
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-09 12:52:27 +0100

    continuation

--- lesson11.py
+++ lesson11.py
@@ -113,15 +113,42 @@ class  Customer(object):
         self.customer_id = customer_id
 
     def display_cart(self):
-        print('I\'m a string that stands in for the contents of your shopping cart')
+        print('I\'m a string that stands in for the contents of your shopping cart.')
 
 class ReturningCustomer(Customer):
     def display_order_history(self):
-        print('I\'m a string thatstands in for your order history')
+        print('I\'m a string thatstands in for your order history.')
 
 monthy_python = ReturningCustomer('ID: 12345')
 monthy_python.display_cart()
 monthy_python.display_order_history()
+
+#Inheritance syntax
+class Shape(object):
+    def __init__(self, number_of_sides):
+        self.number_of_sides = number_of_siides
+
+class Triangle(Shape):
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

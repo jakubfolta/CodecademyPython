3c72040bfaf884b656ce681840ee2012a6a42b9c
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-11 03:52:23 +0100

    continuation

--- lesson11.py
+++ lesson11.py
@@ -179,7 +179,10 @@ print(milton.full_time_wage(10))
 #Class basics
 class Triangle(object):
     def __init__(self, angle1, angle2, angle3):
-        pass
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+        
         
 
 

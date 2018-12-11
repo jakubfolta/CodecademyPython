3f1601d53f9ceafabbaf636623cb9bb2fc5e6233
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-11 04:23:31 +0100

    continuation

--- lesson11.py
+++ lesson11.py
@@ -184,8 +184,25 @@ class Triangle(object):
         self.angle3 = angle3
 
 #Class it up
-
+class Triangle(object):
+    def __init__(self, angle1, angle2, angle3):
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
         
+        
+    number_of_sides = 3
+
+    def check_angles(self):
+        total = [self.angle1, self.angle2, self.angle3]
+        if sum(total) == 180:
+            return True
+        else:
+            return False
+
+triangle = Triangle(32, 43, 66)
+
+print(triangle.check_angles())
 
 
 

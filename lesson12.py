bd0d29f01122dd52b065ce7002fb95563933737a
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-13 04:21:14 +0100

    continuation

--- lesson12.py
+++ lesson12.py
@@ -50,6 +50,22 @@ print(my_car.model)
 print(my_car.color)
 print(my_car.mpg)
 
+#Creating class methods
+class Car(object):
+    condition = 'new'
+
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+
+    def display_car(self):
+        print('This is a {} {} with {} MPG.'.format(self.color, self.model, self.mpg))
+
+my_car = Car('DeLorean', 'silver', 88)
+
+my_car.display_car()
+
 
 
 

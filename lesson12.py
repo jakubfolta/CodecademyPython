e26c7e3e9c91fc2d9d5ac2ef9fd81bac1a5ec71f
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-13 03:45:07 +0100

    continuation

--- lesson12.py
+++ lesson12.py
@@ -19,5 +19,27 @@ class Car(object):
     condition = 'new'
 
 my_car = Car()
+    
+print(my_car.condition)
+
+#Initializing a class
+class Car(object):
+    condition = 'new'
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+
+my_car = Car('DeLorean', 'silver', 88)
 
 print(my_car.condition)
+print(my_car.self)
+
+
+
+
+
+
+
+
+

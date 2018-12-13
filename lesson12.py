37bbe22586ff39405b630ff4759d0d901663d92a
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-13 04:32:45 +0100

    continuation

--- lesson12.py
+++ lesson12.py
@@ -67,6 +67,30 @@ my_car = Car('DeLorean', 'silver', 88)
 my_car.display_car()
 
 #Modifying member variables
+class Car(object):
+    condition = 'new'
+
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+
+    def display_car(self):
+        print('This is a {} {} with {} MPG.'.format(self.color, self.model, self.mpg))
+
+    def drive_car(self):
+        self.condition = 'used'
+        
+    
+my_car = Car('DeLorean', 'silver', 88)
+
+print(my_car.condition)
+
+my_car.drive_car()
+
+print(my_car.condition)
+
+#Inheritance
 
 
 

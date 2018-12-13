c54a27407074d86b1263f42c50c3e20e8f0a26fa
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-13 04:44:01 +0100

    continuation

--- lesson12.py
+++ lesson12.py
@@ -91,7 +91,40 @@ my_car.drive_car()
 print(my_car.condition)
 
 #Inheritance
+class Car(object):
+    condition = 'new'
 
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+
+    def display_car(self):
+        print('This is a {} {} with {} MPG.'.format(self.color, self.model, self.mpg))
+
+    def drive_car(self):
+        self.condition = 'used'
+        
+    
+my_car = Car('DeLorean', 'silver', 88)
+
+print(my_car.condition)
+
+my_car.drive_car()
+
+print(my_car.condition)
+
+class ElectricCar(Car):
+    
+    def __init__(self, model, color, mpg, battery_type):
+        self.model = model
+        self.color = color
+        self.mpg = mpg
+        self.battery_type = battery_type
+
+my_car = ElectricCar('Ford', 'black', 100, 'molten salt')
+
+my_car.display_car()
 
 
 

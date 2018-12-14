0b1b054fa06b439cd0a2f6440a5e4c2e81e27f83
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-14 04:41:12 +0100

    continuation

--- lesson12.py
+++ lesson12.py
@@ -1,3 +1,13 @@
 #File Input/Output
 #The open() function
-my_file = open('output.txt', 'r+')
+my_file = open('output.txt', 'w')
+
+#Writing
+my_list = [i ** 2 for i in range(1, 11)]
+
+my_file = open('output.txt', 'w')
+
+for x in my_list:
+    my_file.write(str(x) + '\n')
+
+my_file.close()

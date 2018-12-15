c7b8762b2db6cfff501d7f813b4fb7bd3cf2623e
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-15 12:11:06 +0100

    continuation

--- lesson12.py
+++ lesson12.py
@@ -42,8 +42,18 @@ write_file = open('text.txt','w')
 read_file = open('text.txt','r')
 
 write_file.write('Not closing files is VERY BAD.')
+write_file.close()
 
 print(read_file.read())
+read_file.close()
+
+
+
+
+
+
+
+
 
 
 
